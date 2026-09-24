c750c4731ad5543023fca0f563e7d7a5f2260fc5
sano-hwa <sano@hasega-wa.jp>
2026-09-24 14:16:47 +0900

    ハンバーガーメニュー

--- js/common.js
+++ js/common.js
@@ -1,11 +1,7 @@
-const gnav = document.querySelector('.gnav');
-const gnavBtn = document.querySelector('.gnav__btn');
+const gnav = document.querySelector(".gnav");
+const gnavBtn = document.querySelector(".gnav__btn");
 
-gnavBtn.addEventListener('click', () => {
-  gnav.classList.toggle('is-active');
-  if (gnav.classList.contains('is-active')) {
-    gnavBtn.textContent = 'CLOSE';
-  } else {
-    gnavBtn.textContent = 'MENU';
-  }
+gnavBtn.addEventListener("click", () => {
+  gnav.classList.toggle("is-active");
+  gnavBtn.classList.toggle("is-active");
 });
